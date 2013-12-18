132576cd1ad69b615656fcc1e61964b7c537a955
Daniel Baelz <daniel.baelz@lysandor.de>
2013-12-18 10:19:39 +0100

    Add InputManager for input handling

--- Secludedness/src/main/java/de/dbaelz/secludedness/MainGame.java
+++ Secludedness/src/main/java/de/dbaelz/secludedness/MainGame.java
@@ -3,12 +3,20 @@ package de.dbaelz.secludedness;
 
 import com.badlogic.gdx.Game;
 
-import de.dbaelz.secludedness.screen.MenuScreen;
+import de.dbaelz.secludedness.manager.InputManager;
 import de.dbaelz.secludedness.screen.StartScreen;
 
 public class MainGame extends Game {
+	private InputManager mInputManager;
+	
 	@Override
 	public void create() {
+		mInputManager = new InputManager();
+		
 		setScreen(new StartScreen(this));
-	}	
+	}
+	
+	public InputManager getInputManager() {
+		return mInputManager;
+	}
 }
---
+++ Secludedness/src/main/java/de/dbaelz/secludedness/manager/InputManager.java
@@ -0,0 +1,62 @@
+package de.dbaelz.secludedness.manager;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Input.Orientation;
+import com.badlogic.gdx.Input.Peripheral;
+
+import de.dbaelz.secludedness.level.Level;
+import de.dbaelz.secludedness.level.Player;
+
+public class InputManager {
+
+	
+	public void updatePlayerPosition(float delta, Level level, Player player) {
+		// TODO: Set controller by controller settings
+		// TODO: Add support for controller
+		
+		int xPosition = player.getPositionX();
+		int yPosition = player.getPositionY();
+		
+		if ((Gdx.app.getType() == ApplicationType.Android) && (Gdx.input.isPeripheralAvailable(Peripheral.Accelerometer))) {
+			float accelX = Gdx.input.getAccelerometerX();
+			float accelY = Gdx.input.getAccelerometerY();
+
+			if (Gdx.input.getNativeOrientation() == Orientation.Portrait) {
+				float rotation = Gdx.input.getRotation() - 180;
+			}
+
+			float sensibility = 4.0f;
+			if (accelX > sensibility) {
+				xPosition += 64;
+			} else if (accelX < -sensibility) {
+				xPosition -= 64;
+			} else if (accelY > sensibility) {
+				yPosition -= 64;
+			} else if (accelY < -sensibility) {
+				yPosition += 64;
+			}
+				
+		} else {
+			if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+				xPosition += 64;
+			} else if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+				xPosition -= 64;
+			} else if (Gdx.input.isKeyPressed(Keys.UP)) {
+				yPosition += 64;
+			} else if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+				yPosition -= 64;
+			}
+		}
+		
+		if (xPosition != player.getPositionX() || yPosition != player.getPositionY()) {
+			if (!level.isColliding(xPosition, yPosition)) {
+				player.setPositionX(xPosition);
+				player.setPositionY(yPosition);
+			}					
+		}
+
+	}	
+
+}
