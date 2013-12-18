2c71ebae9b08dba2dbc59e692d4fe23b16418c6a
Daniel Baelz <daniel.baelz@lysandor.de>
2013-12-18 14:14:33 +0100

    Use event based input when possible, use polling on android with accelerometer

--- Secludedness/src/main/java/de/dbaelz/secludedness/MainGame.java
+++ Secludedness/src/main/java/de/dbaelz/secludedness/MainGame.java
@@ -3,20 +3,12 @@ package de.dbaelz.secludedness;
 
 import com.badlogic.gdx.Game;
 
-import de.dbaelz.secludedness.manager.InputManager;
 import de.dbaelz.secludedness.screen.StartScreen;
 
 public class MainGame extends Game {
-	private InputManager mInputManager;
-	
 	@Override
 	public void create() {
-		mInputManager = new InputManager();
-		
 		setScreen(new StartScreen(this));
 	}
-	
-	public InputManager getInputManager() {
-		return mInputManager;
-	}
+
 }
--- Secludedness/src/main/java/de/dbaelz/secludedness/manager/InputManager.java
+++ Secludedness/src/main/java/de/dbaelz/secludedness/manager/InputManager.java
@@ -1,21 +1,85 @@
 package de.dbaelz.secludedness.manager;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Input.Orientation;
 import com.badlogic.gdx.Input.Peripheral;
+import com.badlogic.gdx.InputProcessor;
 
 import de.dbaelz.secludedness.level.Level;
 import de.dbaelz.secludedness.level.Player;
 
-public class InputManager {
-
+public class InputManager implements InputProcessor {
+	private Level mLevel;
+	private Player mPlayer;
 	
-	public void handlePlayerInput(float delta, Level level, Player player) {
-		// TODO: Set controller by controller settings
-		// TODO: Add support for controller
+	public InputManager(Level level, Player player) {
+		mLevel = level;
+		mPlayer = player;
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		int xPosition = mPlayer.getPositionX();
+		int yPosition = mPlayer.getPositionY();
 		
+		if (keycode == Keys.RIGHT) {
+			xPosition += 64;
+		} else if (keycode == Keys.LEFT) {
+			xPosition -= 64;
+		} else if (keycode == Keys.UP) {
+			yPosition += 64;
+		} else if (keycode == Keys.DOWN) {
+			yPosition -= 64;
+		}
+		
+		handlePlayer(xPosition, yPosition);
+		return true;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}	
+
+	public void pollPlayerInput(float delta, Level level, Player player) {
 		int xPosition = player.getPositionX();
 		int yPosition = player.getPositionY();
 		
@@ -23,11 +87,13 @@ public class InputManager {
 			float accelX = Gdx.input.getAccelerometerX();
 			float accelY = Gdx.input.getAccelerometerY();
 
+			// Game is in landscape, so rotate axes when default orientation is portrait
 			if (Gdx.input.getNativeOrientation() == Orientation.Portrait) {
-				float rotation = Gdx.input.getRotation() - 180;
+				accelX = Gdx.input.getAccelerometerY();
+				accelY = Gdx.input.getAccelerometerX();
 			}
 
-			float sensibility = 4.0f;
+			float sensibility = 5.0f;
 			if (accelX > sensibility) {
 				xPosition += 64;
 			} else if (accelX < -sensibility) {
@@ -50,19 +116,22 @@ public class InputManager {
 			}
 		}
 		
-		if (xPosition != player.getPositionX() || yPosition != player.getPositionY()) {
-			if (level.isCollidingWithDamage(xPosition, yPosition)) {
-				player.changeHealthBy(-1);
-				player.setPositionX(xPosition);
-				player.setPositionY(yPosition);
-			} else if (level.isCollidingWithPortal(xPosition, yPosition)) {
-				player.resetPositionToStart();
-			} else if (!level.isCollidingWithForeground(xPosition, yPosition)) {
-				player.setPositionX(xPosition);
-				player.setPositionY(yPosition);
+		handlePlayer(xPosition, yPosition);
+	}
+
+	
+	private void handlePlayer(int xPosition, int yPosition) {
+		if (xPosition != mPlayer.getPositionX() || yPosition != mPlayer.getPositionY()) {
+			if (mLevel.isCollidingWithDamage(xPosition, yPosition)) {
+				mPlayer.changeHealthBy(-1);
+				mPlayer.setPositionX(xPosition);
+				mPlayer.setPositionY(yPosition);
+			} else if (mLevel.isCollidingWithPortal(xPosition, yPosition)) {
+				mPlayer.resetPositionToStart();
+			} else if (!mLevel.isCollidingWithForeground(xPosition, yPosition)) {
+				mPlayer.setPositionX(xPosition);
+				mPlayer.setPositionY(yPosition);
 			}					
 		}
-
-	}	
-
+	}
 }
