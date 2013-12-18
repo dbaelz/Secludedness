b771f8406a2c594ce0d4fa291561268bc9edb69b
Daniel Baelz <daniel.baelz@lysandor.de>
2013-12-18 18:40:32 +0100

    Add new map and texture including exit layer

--- Secludedness-android/assets/textures/texturesV1.tsx
+++ Secludedness-android/assets/textures/texturesV1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="texturesV1" tilewidth="64" tileheight="64">
- <image source="texture.png" width="320" height="64"/>
+ <image source="texture.png" width="384" height="64"/>
 </tileset>
