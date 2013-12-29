7f109161f60b012e357db5e3d9a428f650400be4
Daniel Baelz <daniel.baelz@lysandor.de>
2013-12-29 18:51:38 +0100

    Refactor and expand GPGS implementation for android

--- Secludedness-android/src/de/dbaelz/secludedness/MainActivity.java
+++ Secludedness-android/src/de/dbaelz/secludedness/MainActivity.java
@@ -5,23 +5,48 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.google.example.games.basegameutils.GameHelper;
+import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
 
 import de.dbaelz.secludedness.MainGame;
 import de.dbaelz.secludedness.manager.AndroidGPGSManager;
+import de.dbaelz.secludedness.manager.GPGSManager;
 
-public class MainActivity extends AndroidApplication {
+public class MainActivity extends AndroidApplication implements GameHelperListener {
 	private GameHelper mGameHelper;
+	private GPGSManager mGPGSManager;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);        
         mGameHelper = new GameHelper(this);
+        mGPGSManager = new AndroidGPGSManager(mGameHelper, this);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
 		cfg.useGL20 = true;
 		cfg.useWakelock = true;
 		cfg.useAccelerometer = true;
         
-        initialize(new MainGame(new AndroidGPGSManager()), cfg);
+		initialize(new MainGame(mGPGSManager), cfg);
+        mGameHelper.setup(this);
     }
+    
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	mGameHelper.onStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	mGameHelper.onStop();
+    }
+    
+	@Override
+	public void onSignInFailed() {
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+	}
 }
--- Secludedness-android/src/de/dbaelz/secludedness/manager/AndroidGPGSManager.java
+++ Secludedness-android/src/de/dbaelz/secludedness/manager/AndroidGPGSManager.java
@@ -1,41 +1,55 @@
 package de.dbaelz.secludedness.manager;
 
+import android.app.Activity;
+
+import com.google.example.games.basegameutils.GameHelper;
+
 public class AndroidGPGSManager implements GPGSManager {
-
+	private GameHelper mGameHelper;
+	private Activity mActivity;
+	
+	public AndroidGPGSManager(GameHelper gamehelper, Activity activity) {
+		mGameHelper = gamehelper;
+		mActivity = activity;
+	}
+	
 	@Override
-	public boolean isUserSignedIn() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isSignedIn() {
+		return mGameHelper.isSignedIn();
 	}
 
 	@Override
-	public void loginUser() {
-		// TODO Auto-generated method stub
-		
+	public void signIn() {
+		mGameHelper.beginUserInitiatedSignIn();
 	}
 
+	@Override
+	public void signOut() {
+		mGameHelper.signOut();
+	}
+	
 	@Override
 	public void unlockAchievement(String achievementID) {
-		// TODO Auto-generated method stub
-		
+		mGameHelper.getGamesClient().unlockAchievement(achievementID);
+	}
+	
+	@Override
+	public void incrementAchievement(String achievementID, int steps) {
+		mGameHelper.getGamesClient().incrementAchievement(achievementID, steps);
 	}
 
 	@Override
 	public void getAchievements() {
-		// TODO Auto-generated method stub
-		
+		mActivity.startActivityForResult(mGameHelper.getGamesClient().getAchievementsIntent(), 10);
 	}
 
 	@Override
-	public void submitScore() {
-		// TODO Auto-generated method stub
-		
+	public void submitScore(int score) {
+		//mGameHelper.getGamesClient().submitScore(LEADERBORD_ID, score);
 	}
 
 	@Override
 	public void getLeaderboard() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
