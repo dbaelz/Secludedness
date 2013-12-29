27f3b87281e36dc24316809bde4ef7be3978ff99
Daniel Baelz <daniel.baelz@lysandor.de>
2013-12-29 18:45:41 +0100

    Refactor GPGS interface

--- Secludedness/src/main/java/de/dbaelz/secludedness/manager/GPGSManager.java
+++ Secludedness/src/main/java/de/dbaelz/secludedness/manager/GPGSManager.java
@@ -1,10 +1,12 @@
 package de.dbaelz.secludedness.manager;
 
 public interface GPGSManager {
-	public boolean isUserSignedIn();
-	public void loginUser();
+	public boolean isSignedIn();
+	public void signIn();
+	public void signOut();
 	public void unlockAchievement(String achievementID);
+	public void incrementAchievement(String achievementID, int steps);
 	public void getAchievements();
-	public void submitScore();
+	public void submitScore(int score);
 	public void getLeaderboard();
 }
