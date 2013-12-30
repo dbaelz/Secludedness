90fdad124694482a0e7e4960f35dc11e68b38589
Daniel Baelz <daniel.baelz@lysandor.de>
2013-12-30 12:04:30 +0100

    Add new tile set and changed maps

---
+++ Secludedness-android/assets/textures/grey.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grey" tilewidth="64" tileheight="64">
+ <image source="grey.png" width="256" height="192"/>
+</tileset>
--- Secludedness-android/assets/textures/texturesV1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="texturesV1" tilewidth="64" tileheight="64">
- <image source="texture.png" width="448" height="64"/>
-</tileset>
