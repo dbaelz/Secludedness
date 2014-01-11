7fa080393cc5565d2a2f5b7495d196c7040b53b7
Daniel Baelz <daniel.baelz@lysandor.de>
2014-01-12 00:12:44 +0100

    GPGS: Add debug output for desktop and html project

--- Secludedness-desktop/src/main/java/de/dbaelz/secludedness/manager/DesktopGPGSManager.java
+++ Secludedness-desktop/src/main/java/de/dbaelz/secludedness/manager/DesktopGPGSManager.java
@@ -1,58 +1,59 @@
 package de.dbaelz.secludedness.manager;
 
+import com.badlogic.gdx.Gdx;
+
 
 public class DesktopGPGSManager implements GPGSManager {
-
+	private final String GPGS_MANAGER = "GPGS MANAGER";
+	
 	@Override
 	public boolean isSignedIn() {
-		// TODO Auto-generated method stub
-		return false;
+		Gdx.app.log(GPGS_MANAGER, "isSignedIn TRUE");
+		return true;
 	}
 
 	@Override
 	public void signIn() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "signIn");
 	}
 
 	@Override
 	public void signOut() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "signOut");
 	}
 	
 	@Override
 	public void unlockAchievement(String achievementID) {
-		System.out.println("PLACEHOLDER: UNLOCK ACHIEVEMENT " + achievementID);
+		Gdx.app.log(GPGS_MANAGER, "UNLOCK ACHIEVEMENT: " + achievementID);
 	}
 
 	@Override
 	public void incrementAchievement(String achievementID, int steps) {
-		System.out.println("PLACEHOLDER: INCREMENT ACHIEVEMENT " + achievementID + " BY " + steps + " STEPS");	}
+		Gdx.app.log(GPGS_MANAGER, "INCREMENT ACHIEVEMENT: " + achievementID + " BY " + steps + " STEPS");
+	}
 	
 	@Override
 	public void getAchievements() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "getAchievements");
 	}
 
 	@Override
 	public void submitScore(String leaderboardID, int score) {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "Leaderboard " +  leaderboardID + " submit score " + score);
 	}
 
 	@Override
 	public void getLeaderboard(String leaderboardID) {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "getLeaderboard " + leaderboardID);
 	}
 
 	@Override
 	public void saveCampaignToCloud(int campaignLevel, int campaignScore) {
+		Gdx.app.log(GPGS_MANAGER, "saveCampaignToCloud: Campaign Level " + campaignLevel + " Campaign Score " + campaignScore);
 	}
 
 	@Override
 	public void loadCampaignFromCloud() {
+		Gdx.app.log(GPGS_MANAGER, "loadCampaignFromCloud");
 	}
 }
--- Secludedness-html/src/de/dbaelz/secludedness/manager/GWTGPGSManager.java
+++ Secludedness-html/src/de/dbaelz/secludedness/manager/GWTGPGSManager.java
@@ -1,60 +1,58 @@
 package de.dbaelz.secludedness.manager;
 
-public class GWTGPGSManager implements GPGSManager {
+import com.badlogic.gdx.Gdx;
 
+public class GWTGPGSManager implements GPGSManager {
+	private final String GPGS_MANAGER = "GPGS MANAGER";
+	
 	@Override
 	public boolean isSignedIn() {
-		// TODO Auto-generated method stub
-		return false;
+		Gdx.app.log(GPGS_MANAGER, "isSignedIn TRUE");
+		return true;
 	}
 
 	@Override
 	public void signIn() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "signIn");
 	}
 
 	@Override
 	public void signOut() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "signOut");
 	}
 	
 	@Override
 	public void unlockAchievement(String achievementID) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	public void incrementAchievement(String achievementID, int steps) {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "UNLOCK ACHIEVEMENT: " + achievementID);
 	}
 
 	@Override
+	public void incrementAchievement(String achievementID, int steps) {
+		Gdx.app.log(GPGS_MANAGER, "INCREMENT ACHIEVEMENT: " + achievementID + " BY " + steps + " STEPS");
+	}
+	
+	@Override
 	public void getAchievements() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "getAchievements");
 	}
 
 	@Override
 	public void submitScore(String leaderboardID, int score) {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "Leaderboard " +  leaderboardID + " submit score " + score);
 	}
 
 	@Override
 	public void getLeaderboard(String leaderboardID) {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log(GPGS_MANAGER, "getLeaderboard " + leaderboardID);
 	}
 
 	@Override
 	public void saveCampaignToCloud(int campaignLevel, int campaignScore) {
+		Gdx.app.log(GPGS_MANAGER, "saveCampaignToCloud: Campaign Level " + campaignLevel + " Campaign Score " + campaignScore);
 	}
 
 	@Override
 	public void loadCampaignFromCloud() {
+		Gdx.app.log(GPGS_MANAGER, "loadCampaignFromCloud");
 	}
 }
